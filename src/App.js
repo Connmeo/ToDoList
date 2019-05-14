a48b07a88ebdc067e6ffe8b7fe6f0ce53722a634
Fedin Dima <dima.groot.04@gmail.com>
2019-05-14 22:30:43 +0300

    fix errors in App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
 import React, { useState } from 'react';
-import connect from '@vkontakte/vkui-connect';
 import { View } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 import ToDoList from './panels/ToDoList';
 
-const App = ({}) => {
+const App = () => {
+  const [todoValue, setTodoValue] = useState('');
+  const [todos, setTodo] = useState([]);
+  // activePanel не просто можно, а нужно делать через useState
+  // если бы компонент App у тебя был классом, тогда надо было бы использовать this.state
+  const [activePanel] = useState('todolist');
 
+  // кстати, id панелей и просто строки в JS чувствительны к регистру, другими словами 'ToDoList' !== 'todolist'
 
-	//Можно ли тут state.activePanel тоже через useState сделать?Если можно,то как?
-return (
- this.state = {
-    activePanel: 'ToDoList'  
-  }
-
- 
-const [todoValue, setTodoValue] = useState('');
-const [todos, setTodo] = useState([]);
-//const [activePanel = useState([])]
-
-
-
-  <View activePanel={this.state.activePanel}>
-    <ToDoList
-      id="todolist"
-      todoValue={todoValue}
-      setTodoValue={setTodoValue}
-      todos={todos}
-      setTodo={setTodo}
-    />
-  </View>
-);
-}
+  return (
+    <View activePanel={activePanel}>
+      <ToDoList
+        id="todolist"
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+        todos={todos}
+        setTodo={setTodo}
+      />
+    </View>
+  );
+};
 
 export default App;
-
-
